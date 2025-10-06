ec72eef22466265cb30878f26b00bd2a4a379da2
Partybox98 <147873530+Partybox98@users.noreply.github.com>
2025-10-06 12:35:14 -0700

    fixed camera jitter

--- Pale Dawn/Assets/Scripts/CameraFl.cs
+++ Pale Dawn/Assets/Scripts/CameraFl.cs
@@ -7,12 +7,16 @@ public class Camera : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, new Vector3(followPos.position.x,followPos.position.y,transform.position.z), speed);
+        
+    }
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(followPos.position.x, followPos.position.y, transform.position.z), speed);
     }
 }
