c4a81cd69a9f5e6669179e5faece21884df1caff
Partybox98 <147873530+Partybox98@users.noreply.github.com>
2025-12-02 13:55:19 -0800

    proj test

--- Pale Dawn/Assets/Scripts/FollowProjectile.cs
+++ Pale Dawn/Assets/Scripts/FollowProjectile.cs
@@ -1,15 +1,23 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class FollowProjectile : Enemy
 {
+    private Vector2 facing;
+    protected override void ExtraStart()
+    {
+        facing = Vector2.zero;
+    }
     protected override void move()
     {
         
     }
     protected override void FixedUpdate()
     {
-        Vector2 facing = new Vector2(transform.eulerAngles.x,transform.eulerAngles.y);
+        if (target != null)
+        {
+            // rb.transform.LookAt(target.transform);
+            facing = Vector2.MoveTowards(Vector2.zero,target.transform.position,1);
+        }
         rb.linearVelocity = facing*speed;
     }
 }
