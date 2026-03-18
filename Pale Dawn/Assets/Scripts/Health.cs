5746341cfcf5e535496c09001e06dc3afcd274cd
Partybox98 <kirbymmo@gmail.com>
2026-03-18 13:19:09 -0700

    health update

--- Pale Dawn/Assets/Scripts/Health.cs
+++ Pale Dawn/Assets/Scripts/Health.cs
@@ -11,7 +11,7 @@ public class Health : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        //comment 2
     }
 
     // Update is called once per frame
