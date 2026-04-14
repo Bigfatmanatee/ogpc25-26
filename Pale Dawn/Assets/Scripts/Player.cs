d521c5a21c7c0c28020badee9389989f4614c96f
plubos <67494692+Bigfatmanatee@users.noreply.github.com>
2026-04-14 13:31:04 -0700

    merge

--- Pale Dawn/Assets/Scripts/Player.cs
+++ Pale Dawn/Assets/Scripts/Player.cs
@@ -48,13 +48,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float DsBoost;
     [SerializeField] private float maxInvSec;
     [SerializeField] private float dashCooldown;
-<<<<<<< Updated upstream
+
     [SerializeField] private float maxGravTime;
-=======
     [SerializeField] private GameObject dashParticles;
     [SerializeField] private float dashParticleTime = 0.5f;
     [SerializeField] private GameObject pogoParticles;
->>>>>>> Stashed changes
+
     private float dashTime = 0;
     private float gravTime = 99;
     private bool groundTouch = true;
@@ -248,12 +247,12 @@ public class Player : MonoBehaviour
 
         if (m_DashAction.WasPressedThisFrame() && dashTime >= dashCooldown)
         {
-<<<<<<< Updated upstream
+
             float xTest = 0.65f;
-=======
+
             StartCoroutine(DashParticles());
-            float xTest = 0.5f;
->>>>>>> Stashed changes
+
+
             if (m_PlayerMovement.x >= 0.3)
             {
                 xTest = m_PlayerMovement.x;
